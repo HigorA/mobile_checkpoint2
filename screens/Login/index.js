a96e6404edfe287e78d5b6aa0af6d8bf47357f98
higor <higor.a.santos12@hotmail.com>
2023-04-30 16:27:36 -0300

    Modificação do formulário

--- screens/Login/index.js
+++ screens/Login/index.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { View } from "react-native";
-import { Button, Text, StyleSheet} from "react-native";
+import { View, Button, Text, StyleSheet, TextInput, Pressable } from "react-native";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -12,9 +11,20 @@ export default function Login() {
   };
 
   return (
-    <View>
-      <Text>Login</Text>
-      <form style={styles.form} onSubmit={handleSubmit}>
+    <View style={styles.container}>
+      <Text style={styles.buttonText}>Login</Text>
+      <View style={styles.form}>
+        <View>
+            <Text>E-mail</Text>
+            <TextInput style={styles.input} />
+        </View>
+        <View>
+            <Text>Password</Text>
+            <TextInput style={styles.input}/>
+        </View>
+        <Pressable style={styles.button}><Text style={styles.buttonText}>Login</Text></Pressable>
+      </View>
+      {/* <form style={styles.form} onSubmit={handleSubmit}>
         <label>
           Email:
           <input style={styles.input}
@@ -23,7 +33,6 @@ export default function Login() {
             onChange={(event) => setEmail(event.target.value)}
           />
         </label>
-        <br />
         <label>
           Senha:
           <input style={styles.input}
@@ -32,9 +41,51 @@ export default function Login() {
             onChange={(event) => setPassword(event.target.value)}
           />
         </label>
-        <br />
-        <Button style={styles.button} type="submit">Login</Button>
-      </form>
+        <Button style={styles.button} type="submit" title="Login" />
+      </form> */}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+    },
+
+    input: {
+        marginBottom: 10,
+        padding: 10,
+        width: 300,
+        borderRadius: 5,
+        borderWidth: 5,
+        borderStyle: 'solid',
+        borderColor: 'black'
+    },
+
+    button: {
+        backgroundColor: "#d4cde8",
+        color: '#fff',
+        border: 'none',
+        borderRadius: 5,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    buttonText: {
+        fontWeight: '500',
+        fontSize: 20,
+    }
+});
--- screens/Register/index.js
+++ screens/Register/index.js
@@ -1,36 +1,68 @@
-import { useState } from "react";
-import { View } from "react-native";
-import { Button, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet, TextInput, Pressable } from "react-native";
 
 
 export default function Register() {
     
 
     return (
-        <View>
-            <Text>Cadastro</Text>
-            <form style={styles.form} onSubmit={handleRegisterSubmit}>
-                <label>
-                        Email:
-                    <input style={styles.input}
-                    type="email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                    />
-                </label>
-                <br />
+        <View style={styles.container}>
+            <Text style={styles.buttonText}>Register</Text>
+            <View style={styles.form}>
+                <View>
+                    <Text>E-mail</Text>
+                    <TextInput style={styles.input} />
+                </View>
+            </View>
 
-                <label>
-                    Senha:
-                    <input style={styles.input}
-                    type="password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                    />
-                </label>
-                <br />
-                <Button style={styles.button} type="submit">Cadastro</Button>
-            </form>
-        </View>
+            <View>
+                <Text>Password</Text>
+                <TextInput style={styles.input}/>
+            </View>
+            <Pressable style={styles.button}><Text style={styles.buttonText}>Register</Text></Pressable>
+        </View>    
+        
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+    },
+
+    input: {
+        marginBottom: 10,
+        padding: 10,
+        width: 300,
+        borderRadius: 5,
+        borderWidth: 5,
+        borderStyle: 'solid',
+        borderColor: 'black'
+    },
+
+    button: {
+        backgroundColor: "#d4cde8",
+        color: '#fff',
+        border: 'none',
+        borderRadius: 5,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    buttonText: {
+        fontWeight: '500',
+        fontSize: 20,
+    }
+});
